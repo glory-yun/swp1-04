e4668098433b83925a9993b9dfa8f33803b6af3e
glory-yun <yyk1797@kookmin.ac.kr>
2020-06-19 22:15:35 +0900

    "WSGI: 두 수의 합과 곱"의 conf 파일 및 소스 코드 파일

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+    <body>
+        <form action="">
+            a=<input type="number" name="a">
+            b=<input type="number" name="b"><br><br>
+            <input type="submit"><br><br>
+        </form>
+        sum=%(sum)d <br><br>
+        mul=%(mul)d
+    </body>
+</html>
+"""
+
+

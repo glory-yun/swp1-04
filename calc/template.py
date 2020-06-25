1e740de8428c878d32d946180c37b62bccaaaae6
glory-yun <yyk1797@kookmin.ac.kr>
2020-06-25 22:03:29 +0900

    issue #6 1.template.py 에서 %(sum)d %(mul)d -> %(sum)s %(mul)s 로 변경 2.calc.py 에서 "if a.isdigit() and b.isdigit()"을 삭제하고, "try:",   " except:         sum = "input 'number'"         mul = "input 'number'" 을 추가함

--- calc/template.py
+++ calc/template.py
@@ -6,8 +6,8 @@ html = """
             b=<input type="number" name="b"><br><br>
             <input type="submit"><br><br>
         </form>
-        sum=%(sum)d <br><br>
-        mul=%(mul)d
+        sum=%(sum)s <br><br>
+        mul=%(mul)s
     </body>
 </html>
 """
